259aa975627c7307044bd30ff3336eee7d1a5784
ShinYooJung <shinyoojung0@gmail.com>
2021-12-17 01:43:45 +0900

    review listview

---
+++ app/src/main/java/com/example/gymradar/ReviewAdapterData.java
@@ -0,0 +1,31 @@
+package com.example.gymradar;
+
+public class ReviewAdapterData {
+    private String userId;
+    private float starPoint;
+    private String content;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public float getStarPoint() {
+        return starPoint;
+    }
+
+    public void setStarPoint(float starPoint) {
+        this.starPoint = starPoint;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
